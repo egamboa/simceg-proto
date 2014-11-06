55692a2587450c7a32fa087442df78397b522f33
Esteban Gamboa <estebang@thehangar.cr>
2014-11-05 23:01:00 -0600

    Arreglando Roles

--- simceg/grails-app/controllers/org/una/simceg/EstudianteController.groovy
+++ simceg/grails-app/controllers/org/una/simceg/EstudianteController.groovy
@@ -4,8 +4,9 @@ package org.una.simceg
 
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
-
+import grails.plugin.springsecurity.annotation.Secured
 @Transactional(readOnly = true)
+@Secured(['ROLE_ADMIN'])
 class EstudianteController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
